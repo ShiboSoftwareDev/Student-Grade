6451932a98d78ede2cfd1da48d4516579504b74e
Ahmed Shebani <ahmedsheb2003@yahoo.com>
2024-12-15 15:30:30 +0200

    v2

--- Student Grade/Form1.Designer.cs
+++ Student Grade/Form1.Designer.cs
@@ -35,8 +35,8 @@
             lstMarks = new ListBox();
             grpGrade = new GroupBox();
             lblGradeResult = new Label();
-            lblMinResult = new Label();
             lblMaxResult = new Label();
+            lblMinResult = new Label();
             lblGrade = new Label();
             lblMin = new Label();
             lblMax = new Label();
@@ -58,9 +58,9 @@
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(227, 29);
+            btnAdd.Location = new Point(212, 30);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(75, 23);
+            btnAdd.Size = new Size(137, 23);
             btnAdd.TabIndex = 1;
             btnAdd.Text = "Add";
             btnAdd.UseVisualStyleBackColor = true;
@@ -69,9 +69,9 @@
             // grpMarks
             // 
             grpMarks.Controls.Add(lstMarks);
-            grpMarks.Location = new Point(68, 96);
+            grpMarks.Location = new Point(58, 86);
             grpMarks.Name = "grpMarks";
-            grpMarks.Size = new Size(281, 202);
+            grpMarks.Size = new Size(291, 230);
             grpMarks.TabIndex = 2;
             grpMarks.TabStop = false;
             grpMarks.Text = "Marks";
@@ -80,23 +80,23 @@
             // 
             lstMarks.FormattingEnabled = true;
             lstMarks.ItemHeight = 15;
-            lstMarks.Location = new Point(16, 47);
+            lstMarks.Location = new Point(16, 32);
             lstMarks.Name = "lstMarks";
-            lstMarks.Size = new Size(228, 124);
+            lstMarks.Size = new Size(259, 184);
             lstMarks.TabIndex = 0;
             lstMarks.TabStop = false;
             // 
             // grpGrade
             // 
             grpGrade.Controls.Add(lblGradeResult);
-            grpGrade.Controls.Add(lblMinResult);
             grpGrade.Controls.Add(lblMaxResult);
+            grpGrade.Controls.Add(lblMinResult);
             grpGrade.Controls.Add(lblGrade);
             grpGrade.Controls.Add(lblMin);
             grpGrade.Controls.Add(lblMax);
-            grpGrade.Location = new Point(75, 315);
+            grpGrade.Location = new Point(58, 333);
             grpGrade.Name = "grpGrade";
-            grpGrade.Size = new Size(274, 134);
+            grpGrade.Size = new Size(291, 134);
             grpGrade.TabIndex = 3;
             grpGrade.TabStop = false;
             grpGrade.Text = "Grade";
@@ -104,31 +104,37 @@
             // lblGradeResult
             // 
             lblGradeResult.BorderStyle = BorderStyle.FixedSingle;
-            lblGradeResult.Location = new Point(168, 87);
+            lblGradeResult.ImageAlign = ContentAlignment.BottomLeft;
+            lblGradeResult.Location = new Point(175, 90);
             lblGradeResult.Name = "lblGradeResult";
             lblGradeResult.Size = new Size(100, 23);
             lblGradeResult.TabIndex = 5;
-            // 
-            // lblMinResult
-            // 
-            lblMinResult.BorderStyle = BorderStyle.FixedSingle;
-            lblMinResult.Location = new Point(168, 59);
-            lblMinResult.Name = "lblMinResult";
-            lblMinResult.Size = new Size(100, 23);
-            lblMinResult.TabIndex = 4;
+            lblGradeResult.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // lblMaxResult
             // 
             lblMaxResult.BorderStyle = BorderStyle.FixedSingle;
-            lblMaxResult.Location = new Point(168, 33);
+            lblMaxResult.ImageAlign = ContentAlignment.BottomLeft;
+            lblMaxResult.Location = new Point(175, 26);
             lblMaxResult.Name = "lblMaxResult";
             lblMaxResult.Size = new Size(100, 23);
             lblMaxResult.TabIndex = 3;
+            lblMaxResult.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblMinResult
+            // 
+            lblMinResult.BorderStyle = BorderStyle.FixedSingle;
+            lblMinResult.ImageAlign = ContentAlignment.BottomLeft;
+            lblMinResult.Location = new Point(175, 59);
+            lblMinResult.Name = "lblMinResult";
+            lblMinResult.Size = new Size(100, 23);
+            lblMinResult.TabIndex = 4;
+            lblMinResult.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // lblGrade
             // 
             lblGrade.AutoSize = true;
-            lblGrade.Location = new Point(56, 88);
+            lblGrade.Location = new Point(110, 91);
             lblGrade.Name = "lblGrade";
             lblGrade.Size = new Size(38, 15);
             lblGrade.TabIndex = 2;
@@ -137,7 +143,7 @@
             // lblMin
             // 
             lblMin.AutoSize = true;
-            lblMin.Location = new Point(56, 60);
+            lblMin.Location = new Point(74, 60);
             lblMin.Name = "lblMin";
             lblMin.Size = new Size(74, 15);
             lblMin.TabIndex = 1;
@@ -146,7 +152,7 @@
             // lblMax
             // 
             lblMax.AutoSize = true;
-            lblMax.Location = new Point(56, 34);
+            lblMax.Location = new Point(70, 27);
             lblMax.Name = "lblMax";
             lblMax.Size = new Size(78, 15);
             lblMax.TabIndex = 0;
@@ -154,7 +160,7 @@
             // 
             // btnGrade
             // 
-            btnGrade.Location = new Point(37, 479);
+            btnGrade.Location = new Point(58, 482);
             btnGrade.Name = "btnGrade";
             btnGrade.Size = new Size(132, 23);
             btnGrade.TabIndex = 4;
@@ -164,7 +170,7 @@
             // 
             // btnNew
             // 
-            btnNew.Location = new Point(188, 482);
+            btnNew.Location = new Point(196, 482);
             btnNew.Name = "btnNew";
             btnNew.Size = new Size(75, 23);
             btnNew.TabIndex = 5;
@@ -174,9 +180,9 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(286, 482);
+            btnExit.Location = new Point(277, 482);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(75, 23);
+            btnExit.Size = new Size(72, 23);
             btnExit.TabIndex = 6;
             btnExit.Text = "Exit";
             btnExit.UseVisualStyleBackColor = true;
@@ -186,7 +192,7 @@
             // 
             lblError.AutoSize = true;
             lblError.ForeColor = Color.Red;
-            lblError.Location = new Point(68, 68);
+            lblError.Location = new Point(58, 56);
             lblError.Name = "lblError";
             lblError.Size = new Size(0, 15);
             lblError.TabIndex = 7;
@@ -195,6 +201,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(406, 546);
             Controls.Add(lblError);
             Controls.Add(btnExit);
